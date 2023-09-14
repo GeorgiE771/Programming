1e36ec5b94af8a2fae0b314bb17a45d918594efd
GeorgiE771 <evgeniy_maksimov31072002@mail.ru>
2023-09-14 23:34:30 +0300

    Task2

--- Seminars/Seminar01/Self/Task02/Program.cs
+++ Seminars/Seminar01/Self/Task02/Program.cs
@@ -1,1 +1,13 @@
-﻿// Your code here!
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string first_name = Console.ReadLine();
+        string second_name = Console.ReadLine();
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"{second_name} {first_name} {last_name}");
+    }
+}
