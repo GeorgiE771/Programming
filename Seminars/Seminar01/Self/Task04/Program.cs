4c8410c820392f7d0b835110bbaf67e2abff0b07
GeorgiE771 <evgeniy_maksimov31072002@mail.ru>
2023-09-14 23:36:10 +0300

    Task4

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,12 @@
-﻿// Your code here!
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.WriteLine("Hello");
+        Console.ResetColor();
+    }
+}
